e55a575ebb72f5aa8ec74b831bb496bebfd980f0
Truong Giang <37567982+Gjin41@users.noreply.github.com>
2020-02-28 11:04:14 +0700

    Update cs142-template-processor.js

--- cs142-template-processor.js
+++ cs142-template-processor.js
@@ -2,8 +2,9 @@
 function Cs142TemplateProcessor(template){
     this.fillIn = function(dictionary){
         let temp = template;
-        for(let i of Object.keys(dictionary)){    
-            temp = temp.replace("{{"+i+"}}", dictionary[i]);
+        for(let i of Object.keys(dictionary)){
+            if(template.search(i)!==-1)  
+                temp = temp.replace("{{"+i+"}}", dictionary[i]);
         }
         return temp;
     }
