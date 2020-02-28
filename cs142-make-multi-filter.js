bc17d3601cadedf2c8e4ca5432239eed54137515
Truong Giang <37567982+Gjin41@users.noreply.github.com>
2020-02-28 11:31:38 +0700

    Update cs142-make-multi-filter.js

--- cs142-make-multi-filter.js
+++ cs142-make-multi-filter.js
@@ -1,22 +1,21 @@
+'use strict';
 function cs142MakeMultiFilter(originalArray){
     let currentArray = originalArray;
     return function arrayFilterer (filterCriteria, callback){
       let temp = new Array();
         if(typeof filterCriteria !== 'function')
             return currentArray;
-    
-    
-    for(let i of originalArray){
-        if(filterCriteria(i) === true)
-            temp.push(i);
-    }
 
-    if(typeof callback === 'function'){
+      for(let i of currentArray){
+          if(filterCriteria(i) === true)
+              temp.push(i);
+      }
+
       currentArray = temp;
-      let callthis = callback.bind(originalArray);
-      callthis(currentArray);
-    }
-
-    return arrayFilterer;
+      if(typeof callback === 'function'){
+        let callthis = callback.bind(originalArray);
+        callthis(currentArray);
+      }
+      return arrayFilterer;
     };
 }
